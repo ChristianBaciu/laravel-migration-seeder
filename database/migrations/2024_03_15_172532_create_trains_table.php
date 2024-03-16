1407f0eefb32b991128e0929040d67fcb9f8cd84
ChristianBaciu <christian3110@outlook.it>
2024-03-16 22:07:31 +0100

    database creato

--- database/migrations/2024_03_15_172532_create_trains_table.php
+++ database/migrations/2024_03_15_172532_create_trains_table.php
@@ -9,23 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-
-
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('Azienda');
-            $table->string('Stazione di partenza');
-            $table->string('Stazione di arrivo');
-            $table->string('Orario di partenza');
-            $table->string('Orario di arrivo');
-            $table->string('Codice Treno');
-            $table->string('In orario');
-            $table->string('Cancellato');
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 use Faker\Generator as Faker;
 use App\Models\Train;
 
@@ -13,23 +12,20 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(Faker $Faker): void
+    public function run(Faker $faker): void
     {
-        for($i = 0; $i < 100; $i++ ){
-            $newTrain = new Train();
-
-             $new_Train_object->Azienda = $faker->
-             $new_Train_object->Stazione_di_partenza = $faker->
-             $new_Train_object->Stazione_di_arrivo = $faker->
-             $new_Train_object->Orario_di_partenza = $faker->
-             $new_Train_object->Orario_di_arrivo = $faker->
-             $new_Train_object->Codice_Treno = $faker->
-             $new_Train_object->In_orario = $faker->
-             $new_Train_object->Cancellato = $faker->
-
-
-
-            $newTrain-> save();
+        for ($i = 0; $i < 10; $i++) {
+            Train::create([
+                'azienda' => $faker->company,
+                'stazione_di_partenza' => $faker->city,
+                'stazione_di_arrivo' => $faker->city,
+                'orario_di_partenza' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'orario_di_arrivo' => $faker->time($format = 'H:i:s', $max = 'now'),
+                'codice_treno' => $faker->unique()->ean8,
+                'numero_carrozze' => $faker->numberBetween($min = 1, $max = 10),
+                'in_orario' => $faker->boolean,
+                'cancellato' => $faker->boolean,
+            ]);
         }
     }
 }
