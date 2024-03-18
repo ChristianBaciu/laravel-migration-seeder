89afd6db187bf843c4d75261e01f02891108e9c2
ChristianBaciu <christian3110@outlook.it>
2024-03-18 17:53:17 +0100

    sistemazione function run(Faker $faker)

--- database/migrations/2024_03_15_172532_create_trains_table.php
+++ database/migrations/2024_03_15_172532_create_trains_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda');
-            $table->string('stazione_di_partenza');
-            $table->string('stazione_di_arrivo');
-            $table->time('orario_di_partenza');
-            $table->time('orario_di_arrivo');
-            $table->string('codice_treno');
-            $table->integer('numero_carrozze');
+            $table->string('azienda')->nullable();
+            $table->string('stazione_di_partenza')->nullable();
+            $table->string('stazione_di_arrivo')->nullable();
+            $table->dateTime('orario_di_partenza')->nullable();
+            $table->dateTime('orario_di_arrivo')->nullable();
+            $table->string('codice_treno')->nullable();
+            $table->tinyInteger('numero_carrozze')->nullable();
             $table->boolean('in_orario')->default(true);
             $table->boolean('cancellato')->default(false);
             $table->timestamps();
--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 use Faker\Generator as Faker;
 use App\Models\Train;
 
@@ -15,17 +16,27 @@ class TrainsTableSeeder extends Seeder
     public function run(Faker $faker): void
     {
         for ($i = 0; $i < 10; $i++) {
-            Train::create([
-                'azienda' => $faker->company,
-                'stazione_di_partenza' => $faker->city,
-                'stazione_di_arrivo' => $faker->city,
-                'orario_di_partenza' => $faker->time($format = 'H:i:s', $max = 'now'),
-                'orario_di_arrivo' => $faker->time($format = 'H:i:s', $max = 'now'),
-                'codice_treno' => $faker->unique()->ean8,
-                'numero_carrozze' => $faker->numberBetween($min = 1, $max = 10),
-                'in_orario' => $faker->boolean,
-                'cancellato' => $faker->boolean,
-            ]);
+            $newTrain = new Train();
+             $newTrain->azienda = $faker->company();
+             $newTrain->stazione_di_partenza = $faker->randomElement(['Verona Porta Vescovo', 'Vicenza', 'Verona Porta Nuova']);
+             $newTrain->stazione_di_arrivo = $faker->randomElement(['Treviso Centrale', 'Lonigo', 'S. Bonifacio']);
+             // il parametro '-1 Day' prende la data di oggi togliendo un giorno
+             // il parametro '+1 Day' prende la data di oggi aggiungendo un giorno
+             // aggiungendo anche l'orario
+             $newTrain->orario_di_partenza = $faker->dateTimeBetween('-1 Day', '+1 Day');
+             $newTrain->orario_di_arrivo = $faker->dateTimeBetween('-1 Day', '+1 Day');
+             $newTrain->codice_treno = $faker->randomDigit();
+             $newTrain->numero_carrozze = $faker->randomDigit();
+             $newTrain->in_orario = $faker->boolean();
+             $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+
+            // 'orario_di_partenza' => $faker->time($format = 'H:i:s', $max = 'now'),
+
+            // in questo caso non è importante l'ordine di max o format
+            // $newTrain->orario_di_partenza = $faker->time(max:'now', format:'H:i:s');
+
         }
     }
 }
+
